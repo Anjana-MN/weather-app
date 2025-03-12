5ac02bf47bf52b06416331ba276c38ebeab4dbaa
Anjana-MN <anjanamn98@gmail.com>
2025-03-12 14:51:57 +0530

    Revert "implemented spring security and encryption"
    This reverts commit 5840232789880dc52faae259ea999bb6adac6966.

--- weatherforecast/src/main/java/com/project/weatherforecast/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.project.weatherforecast.service;
-
-import com.project.weatherforecast.entity.UserEntity;
-
-public interface UserService {
-
-    UserEntity addUser(UserEntity user);
-}
