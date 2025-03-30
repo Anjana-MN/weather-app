dabfc796b8a6ccadbdf49edb90fa0f4810239b27
Anjana-MN <anjanamn98@gmail.com>
2025-03-30 20:45:57 +0530

    folder structure changed

--- weatherforecast/src/main/java/com/project/weatherforecast/bean/data/Rain.java
+++
@@ -1,9 +0,0 @@
-package com.project.weatherforecast.bean.data;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public class Rain {
-    /** predictedRain */
-    @JsonProperty("3h")
-    private String predictedRain;
-}
